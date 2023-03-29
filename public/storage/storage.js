ecd14450eb22aa5d08b51d80f4cc065743526bfc
Fahmi Muzakki <fahmirockman@gmail.com>
2023-03-29 07:52:46 +0700

    first deploy

---
+++ public/storage/storage.js
@@ -0,0 +1,7 @@
+const storage = {
+  friends: [{ id: 1, name: 'fahmi' }],
+  habitablePlanets: [],
+  launch: {},
+};
+
+module.exports = storage;
--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-const app = require('./app')
+require('module-alias/register');
+const mongoose = require('mongoose');
+const http = require('http');
+const { loadPlanetData } = require('@src/planets/planet.models');
+const app = require('@src/app');
+const { getConfig } = require('../config/config.env');
 
-const port = process.env.PORT || 3000
+const URL = process.env.NODE_ENV !== 'production' ? 'localhost' : getConfig('URL');
+const PORT = getConfig('PORT') || 8000;
+const server = http.createServer(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+mongoose.connection.once('open', () => {
+  console.log('MongoDB Connection is ready');
+});
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+async function start() {
+  await mongoose.connect(getConfig('MONGODB_URL'));
+  await loadPlanetData();
+  server.listen(PORT, () => {
+    console.log(`connected to ${URL}:${PORT}`);
+  });
+}
+start();
